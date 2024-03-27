ff1b3660b2451b1a925f27a83e0fd80d5f05065a
neolbo <wjddlstjs09@naver.com>
2024-03-27 16:03:38 +0900

    Develop Item_Domain

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +13,7 @@ import java.util.List;
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Getter
-@Setter
+//@Setter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -25,4 +26,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    // ItemService 에서 stockQuantity를 가져와 계산 후 setter로 넣는 것이 아님
+    // 응집도 높이기 위해 stockQuantity 가 있는 Item 에 비지니스 로직 작성
+
+    //--비지니스 로직--//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
