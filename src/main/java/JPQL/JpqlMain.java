c6c598157c3488e515aa352584973a2e9b76542e
neolbo <wjddlstjs09@naver.com>
2024-04-16 17:37:35 +0900

    Paging

--- src/main/java/JPQL/JpqlMain.java
+++ src/main/java/JPQL/JpqlMain.java
@@ -16,61 +16,26 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            // 프로젝션
+            // 페이징
 
-            Member member = new Member();
-            member.setUsername("A");
-            member.setAge(10);
-            em.persist(member);
-
-            em.flush();
-            em.clear();
-
-            // entity, embedded type (Address.class) 으로 프로젝션 가능
-
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
-            /**
-             *  쿼리로 뽑아온 result(m) 도 영속성으로 관리 된다!
-             */
-            Member member1 = result.get(0);
-            member1.setAge(20);
-            // ==>> 이름이 A인 member 의 나이가 20으로 db 업데이트
+            for(int i=0; i<100; ++i) {
+                Member member = new Member();
+                member.setUsername("Member" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush();
             em.clear();
 
-            // distinct 로 중복 제거도 가능  select distinct ~~ from ~~
+            List<Member> resultList = em.createQuery("select m from Member as m order by m.age desc", Member.class)
+                    .setFirstResult(5)      // 0번 index 부터 가능
+                    .setMaxResults(10)
+                    .getResultList();
 
-            /**
-             *  스칼라 타입 프로젝션에서 값 가져오는 방법
-             */
-            // 방법 1. 타입 캐스팅해서 사용
-        /*
-            List resultList = em.createQuery("select m.username, m.age from Member as m")
-                    .getResultList();
-            Object o = resultList.get(0);
-            Object[] r = (Object[]) o;
-            System.out.println("name: " + r[0]);
-            System.out.println("age: " + r[1]);
-        */
-            // 방법 2. Object[]로 query 반환
-        /*
-            List<Object[]> resultList = em.createQuery("select m.username, m.age from Member as m")
-                    .getResultList();
-            Object[] r = resultList.get(0);
-            System.out.println("name: " + r[0]);
-            System.out.println("age: " + r[1]);
-        */
-            // 방법 3. new 명령어로 조회 (DTO사용)
-            // 쿼리문 작성을 string 으로 하기에 DTO 클래스명을 패키지까지 직접 작성해줘야함... import 불가
-            // 찾고자하는 데이터와 (순서, 타입) 이 같은 '생성자' 필요
-                    // 순서가 다르거나 타입이 다르면 IllegalStateException 발생
-            List<MemberDTO> resultList = em.createQuery("select new JPQL.MemberDTO(m.username, m.age) from Member as m", MemberDTO.class)
-                    .getResultList();
-            MemberDTO memberDTO = resultList.get(0);
-            System.out.println("memberDTO.getUsername(): " + memberDTO.getUsername());
-            System.out.println("memberDTO.getAge(): " + memberDTO.getAge());
+            for (Member member : resultList) {
+                System.out.println("member.toString() : " + member.toString());
+            }
 
             tx.commit();
         } catch (Exception e) {
