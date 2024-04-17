269ffa3f05b1282953f4f1fdd995883a78431375
neolbo <wjddlstjs09@naver.com>
2024-04-17 15:14:03 +0900

    SubQuery

--- src/main/java/JPQL/JpqlMain.java
+++ src/main/java/JPQL/JpqlMain.java
@@ -16,38 +16,47 @@ public class JpqlMain {
         tx.begin();
 
         try {
-            // 조인
-
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            // sub query
 
             Member member = new Member();
-            member.setUsername("Member");
+            member.setUsername("A");
             member.setAge(10);
-            member.changeTeam(team);
             em.persist(member);
+
 
             em.flush();
             em.clear();
 
-            // 내부 조인
-//            String qlString = "select m from Member as m join m.team t";
+            // 나이가 평균 이상인 멤버
+//            String query = "select m from Member as m where m.age > (select avg(m1.age) from Member as m1)";
 
-            // 외부 조인
-//            String qlString = "select m from Member m left join m.team t";
+            // 한 건이라도 주문한 고객
+//            String query = "select m from Member as m where m = (select o.member from Order o)";
+//            String query = "select m from Member as m where (select count(o) from Order o where m = o.member) > 0";
 
-            // 세타 조인
-//            String qlString = "select count(m) from Member m, Team t where m.username = t.name";
+            // 지원함수
 
-            // on 절 ( join 대상 필터링)
-//            String qlString = "select m from Member m join m.team t on t.name = 'A'";
+            // Exists  -- 서브쿼리에 결과가 존재하면 참
+            // 팀 A 소속인 회원
+//            String query = "select m from Member as m where exists (select t from m.team t where t.name = 'A')";
 
-            // 연관 관계 없는 엔티티끼리의 외부 조인
-            String qlString = "select m from Member m left join Team t on m.username = t.name";
+            // All -- 모두 만족하면 참
+            // 전체 상품 각각의 재고보다 주문량이 많은 주문들
+//            String query = "select o from Order as o where o.orderCount > All (select p.stockQuantity from Product as p)";
 
-            List<Member> resultList = em.createQuery(qlString, Member.class)
+            // Any, Some  -- 조건을 하나라도 만족하면 참
+            // 어떤 팀이든 소속된 회원
+//            String query = "select m from Member as m where m.team = any (select t from Team t)";
+
+            // in  -- 서브 쿼리의 결과 중 하나라도 같은 것이 있으면 참
+            // 속한 팀이 a, b, c 팀 중에 있는 회원
+            String query = "select m from Member as m where m.team = (select t from Team t where t.name in ('a', 'b', 'c') )";
+
+
+
+            List<Member> resultList = em.createQuery(query, Member.class)
                     .getResultList();
+
 
 
 
