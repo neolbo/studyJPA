6a6e4e9fd1f58e1813d1e936f9665798c83c3215
neolbo <wjddlstjs09@naver.com>
2024-05-03 11:54:16 +0900

    Use QueryDsl

--- src/main/java/jpa_ex/shop/repository/order/OrderRepository.java
+++ src/main/java/jpa_ex/shop/repository/order/OrderRepository.java
@@ -1,8 +1,13 @@
 package jpa_ex.shop.repository.order;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jpa_ex.shop.domain.Order;
+import jpa_ex.shop.domain.QMember;
+import jpa_ex.shop.domain.QOrder;
+import jpa_ex.shop.domain.status.OrderStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -10,10 +15,15 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -21,6 +31,34 @@ public class OrderRepository {
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return queryFactory
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                        nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return QMember.member.name.like(nameCond);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllByString(OrderSearch orderSearch) {
